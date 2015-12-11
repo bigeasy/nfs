94a765504ffe3cbd5a7b21f5af27fd6fcb7e6061
Alan Gutierrez <alan@prettyrobots.com>
2015-12-10 23:41:28 -0500

    Set up a listener to evaulate NFS chatter.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 Pure JavaScript Node.js implemenation of an NFSv4 server.
+
+Create a Node.js homeport image.
+
+```console
+homeport create node-0-12-9 --no-cache
+homeport append node-0-12-9 formula/node https://nodejs.org/dist/v0.12.9/node-v0.12.9.tar.gz 35daad301191e5f8dd7e5d2fbb711d081b82d1837d59837b8ee224c256cfe5e4
+homeport append node.0.12.9 formula/apt-get zsh vim man curl rsync git
+homeport append node.0.12.9 formula/locale en_US.UTF-8
+homeport append node.0.12.9 formula/chsh /usr/bin/zsh
+```
+
+*Ed: need instructions on how to build the NFS image.*
+
+```console
+homeport create nfs
+```
+
+Run the `tcp.js` stub.
+
+```console
+homeport run node-0-12-9 -p 2049
+homeport ssh node-0-12-9
+cd ~/git/ecma/packet/nfs
+node tcp.js
+```
+
+Mount using NFS.
+
+```console
+homeport run nfs --link=homeport-node.0.12.9:node
+homeport ssh nfs
+sudo mount -o sec=none $NODE_PORT_2049_TCP_ADDR:/mnt /mnt
+```
---
+++ tcp.js
@@ -0,0 +1,52 @@
+var net = require('net')
+var Reactor = require('reactor')
+var cadence = require('cadence')
+var Delta = require('delta')
+var path = require('path')
+
+var compiler = require('packet/compiler/require')
+var composer = require('packet/compose/parser/inc')
+var filename = path.resolve(__dirname, './generated/alternation.parse.inc.js')
+
+var parsers = composer(compiler(filename), [{
+    type: 'structure',
+    name: 'object',
+    fields: [{
+        type: 'integer',
+        endianess: 'b',
+        bits: 32
+    }]
+}])
+
+function Service () {
+}
+
+Service.prototype.serve = cadence(function (async, status, socket) {
+    async(function () {
+        var loop = async(function () {
+            var data = socket.read()
+            if (data == null) {
+                new Delta(async()).ee(socket).on('readable')
+            } else {
+                return [ loop.break, data ]
+            }
+        })()
+    }, function (data) {
+        console.log(data.length)
+        var frame = data.readUInt32BE(0)
+        var last = !!(frame & ~0x80000000)
+        var length = frame & ~0x80000000
+        console.log(last, length)
+        console.log(data.readUInt32BE(4))
+        console.log(data.toJSON())
+        return []
+    })
+})
+
+var service = new Service
+
+var reactor = new Reactor({ object: service, method: 'serve' })
+
+net.createServer(function (socket) {
+    reactor.push(socket)
+}).listen(2049)
